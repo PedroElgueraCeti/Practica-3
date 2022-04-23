ffb1ee0d3e2da57f06d8145e8ef3aea07ce94e7c
PedroElgueraCeti <a19110148@ceti.mx>
2022-04-23 18:39:13 -0500

    main

---
+++ practica3.py
@@ -0,0 +1,9 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2 #Opencv
+import skimage
+from skimage import io
+import math
+
+img1 = cv2.imread('Ave1.png',1)
+img2 = cv2.imread('Ave2.png',1)
